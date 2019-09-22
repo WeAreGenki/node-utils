7f12de17a83630acfbb8570896036d9e5e769198
Max Milton <max@wearegenki.com>
2019-09-22 09:53:14 +0900

    Add placeholder unit test files

---
+++ src/middleware/__tests__/log.test.ts
@@ -0,0 +1,9 @@
+// import { humanizeSize, log } from '../log';
+
+describe('humanizeSize function', () => {
+  it.todo('write tests');
+});
+
+describe('log middleware', () => {
+  it.todo('write tests');
+});
