0845f86461ea4c697b2d4c2719919003561e1dfb
Max Milton <max@wearegenki.com>
2021-02-27 15:57:17 +1000

    Add deprecation message to parse

--- src/middleware/parse.ts
+++ src/middleware/parse.ts
@@ -4,6 +4,9 @@ import type { Next, Req, Res } from '../types';
  * Parse raw request body data and convert it to JSON.
  * NOTE: This is intentionally simple and should not be considered
  * secure. Do not use anywhere security is important!!
+ *
+ * @deprecated Use the `@polka/parse` package instead;
+ * <https://github.com/lukeed/polka/tree/next/packages/parse>.
  */
 export function parse(req: Req, res: Res, next: Next): void {
   let data = '';
@@ -17,8 +20,9 @@ export function parse(req: Req, res: Res, next: Next): void {
 
     const contentType = req.headers['content-type'];
 
-    if (contentType && contentType.startsWith('application/json')) {
+    if (contentType && contentType.includes('application/json')) {
       try {
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
         req.body = JSON.parse(data);
       } catch (err) {}
     }
